bec773dc84a77c0561ef7ebaec94d42a802209be
MuminaB <barrowmuminatou@gmail.com>
2024-05-28 15:23:47 +0000

    For all to check. It is the modified version of the code. HTML, CSS and other functionalities in the python file have been added

--- Restaurant Management/flask/coffee.py
+++ Restaurant Management/flask/coffee.py
@@ -1,14 +1,17 @@
 # coffee.py
 from flask import Flask, render_template, request, redirect, url_for
-import random
-import string
-from datetime import datetime
+import queries   # type: ignore
+# import random
+# import string
+from datetime import datetime, date
 
 app = Flask(__name__)
 
-def generate_random_order_number(initials, length):
-    random_numbers = ''.join(random.choices(string.digits, k=length))
-    return f"{initials}-{random_numbers}"
+def generate_random_order_number():
+    orderdate = datetime.now()
+    anydate = datetime.strftime(orderdate, '%d%M%S')
+
+    return anydate
 
 def calculate_total(order_list):
     total = 0
@@ -16,68 +19,12 @@ def calculate_total(order_list):
         total += total_price
     return total
 
-order_list = []  # Define order_list as an empty list
+order_list = []  
 
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/order', methods=['POST'])
-# def order():
-#     username = request.form['username']
-#     choice = request.form['choice']
-#     quantity = int(request.form['quantity'])
-
-#     menu = {
-#         "1": "Coffee",
-#         "2": "Cappuccino",
-#         "3": "Tea",
-#         "4": "Hot Chocolate",
-#         "5": "Tropical F.Juice",
-#         "6": "Beef Roll",
-#         "7": "Meat Pie",
-#         "8": "Sandwich",
-#         "9": "Cupcake",
-#         "10": "Spring Roll",
-#     }
-#     item = menu[choice]
-#     price = {
-#         "Coffee": 30.00,
-#         "Cappuccino": 50.00,
-#         "Tea": 40.00,
-#         "Hot Chocolate": 35.00,
-#         "Tropical F.Juice": 60.00,
-#         "Beef Roll": 20.00,
-#         "Meat Pie": 10.00,
-#         "Sandwich": 30.00,
-#         "Cupcake": 25.00,
-#         "Spring Roll": 15.00,
-#     }[item]
-#     total_price = price * quantity
-
-#     if request.form['action'] == 'add_to_cart':
-#         # Add the item to the cart list
-#         order_list.append((item, quantity, price, total_price))
-#         # Redirect back to the index page
-#         return redirect(url_for('index'))
-
-#     elif request.form['action'] == 'checkout':
-#         # Handle checkout process
-#         order_number = generate_random_order_number("Ord", 5)
-#         total_price = calculate_total(order_list)
-        
-#         #to file
-#         with open("C:/Users/user/Desktop/Py/area/order_summary.txt", "a") as to_File:
-#             for item, quantity, unit_price, total_price in order_list:
-#                 to_File.write(f"{username} {order_number} {item}, {quantity}, GHS {unit_price}, GHS {total_price} {datetime.now().strftime('%d/%m/%Y %H:%M')}\n")
-            
-#         return render_template('order_summary.html', order_list=order_list, total_price=total_price, order_number=order_number, username=username)
-
-
-@app.route('/')
+@app.route('/home')
 def mainPage():
-    return render_template('mainPage.html')
+   
+     return render_template('mainPage.html')
     
 
 # @app.route('/staff_page')
@@ -96,33 +43,51 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if username in users and users[username] == password:
-            # Valid login
+          
             return redirect(url_for('staff_page'))
         else:
             return "Invalid credentials. Please try again."
 
     return render_template('login.html')
 
-@app.route('/staff_page')
+@app.route('/staff_page', methods = ['POST', 'GET'])
 def staff_page():
-    order_history = []
-    with open(r"C:\Users\user\restaurant-management\restaurant-management\Restaurant Management\flask\order_summary.txt", "r") as file:
-        for line in file:
-            order_history.append(line.strip().split(' '))
-    return render_template('staff_page.html', order_history=order_history)
+   if request.method == 'POST':
+        choice = request.form['action']
+        if request.form['action'] == "all_orders":
+             result = queries.display_order()
+            #  orderdict ={}
+            #  for OrderID, username, items, quantity, unit_price, total_price, date in result:
+            #         orderdick.setdefault(OrderID, []).append(username)
+             return render_template('staff_page.html', message =  result)
+        elif request.form['action'] == "today_orders":
+                result = queries.displayToday_Order()
+                return render_template('staff_page.html', message =  result)
+        elif request.form['action'] == "yesterday_orders":
+                result = queries.yesterday_Order()
+                return render_template('staff_page.html', message =  result)
+        elif request.form['Date']:
+                domyDate = request.form.get('Date')
+                date = datetime.strptime(domyDate, '%Y-%m-%d')
+                Date = date.strftime('%Y-%m-%d')
+                
+                result = queries.date_Order(Date)
+                return render_template('staff_page.html', message =  result)
+   return render_template('staff_page.html')
 
 
 
 
 @app.route('/customer_page')
 def customer_page():
+     
     return render_template('customer_page.html')
 
 
 @app.route('/order', methods=['POST'])
 def order():
     username = request.form['username']
-    choice = request.form['choice']
+    choice = request.form.get('choice')
     quantity = int(request.form['quantity'])
 
     menu = {
@@ -148,7 +113,7 @@ def order():
         "Meat-Pie": 10.00,
         "Sandwich": 30.00,
         "Cupcake": 25.00,
-        "Spring-Roll": 15.00,
+        "Spring-Roll": 15.00, 
     }[item]
     total_price = price * quantity
 
@@ -161,7 +126,7 @@ def order():
     elif request.form['action'] == 'checkout':
         # Handle checkout process
         order_list.append((item, quantity, price, total_price))
-        order_number = generate_random_order_number("Ord", 5)
+        order_number = generate_random_order_number()
         total_price = calculate_total(order_list)
         
         grand_total = 0  # Initialize the grand total
@@ -172,11 +137,13 @@ def order():
 
 
         #to file
-        with open(r"C:\Users\user\restaurant-management\restaurant-management\Restaurant Management\flask\order_summary.txt", "a") as to_File:
+
             for item, quantity, unit_price, total_price in order_list:
-                to_File.write(f"{username}, {order_number}, {item}, {quantity}, GHS {unit_price}, GHS {total_price}, {datetime.now().strftime('%Y/%m/%d %H:%M')}\n")
-            
-        return render_template('order_summary.html', order_list=order_list, grand_total=grand_total, order_number=order_number, username=username)
+                queries.addorder(order_number, username, item, quantity, unit_price, total_price)
+        
+       
+
+        return render_template('order_summary.html', order_list=order_list, grand_total=grand_total, order_number=order_number, username=username )
 
 
 
